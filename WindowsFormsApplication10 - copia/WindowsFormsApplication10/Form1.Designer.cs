41bd4c682dc5527ea34783aac3f1b598a0de44fc
panixjr <71257032+panixjr@users.noreply.github.com>
2020-09-16 12:37:51 +0200

    Add files via upload

---
+++ WindowsFormsApplication10 - copia/WindowsFormsApplication10/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace WindowsFormsApplication10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ListaAleatorio = new System.Windows.Forms.ListBox();
+            this.Ejercicio2 = new System.Windows.Forms.ListBox();
+            this.Ejercicio3 = new System.Windows.Forms.ListBox();
+            this.Ejercicio4 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.Ejercicio5 = new System.Windows.Forms.ListBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.Ejercicio6 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.Ejercicio7 = new System.Windows.Forms.ListBox();
+            this.Ejercicio8 = new System.Windows.Forms.ListBox();
+            this.Ejercicio9 = new System.Windows.Forms.ListBox();
+            this.Ejercicio10 = new System.Windows.Forms.ListBox();
+            this.Ejercicio11 = new System.Windows.Forms.ListBox();
+            this.Ejercicio12 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(326, 34);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(454, 34);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // ListaAleatorio
+            // 
+            this.ListaAleatorio.FormattingEnabled = true;
+            this.ListaAleatorio.Location = new System.Drawing.Point(68, 63);
+            this.ListaAleatorio.Name = "ListaAleatorio";
+            this.ListaAleatorio.Size = new System.Drawing.Size(102, 95);
+            this.ListaAleatorio.TabIndex = 4;
+            // 
+            // Ejercicio2
+            // 
+            this.Ejercicio2.FormattingEnabled = true;
+            this.Ejercicio2.Location = new System.Drawing.Point(197, 63);
+            this.Ejercicio2.Name = "Ejercicio2";
+            this.Ejercicio2.Size = new System.Drawing.Size(102, 95);
+            this.Ejercicio2.TabIndex = 5;
+            // 
+            // Ejercicio3
+            // 
+            this.Ejercicio3.FormattingEnabled = true;
+            this.Ejercicio3.Location = new System.Drawing.Point(326, 63);
+            this.Ejercicio3.Name = "Ejercicio3";
+            this.Ejercicio3.Size = new System.Drawing.Size(102, 95);
+            this.Ejercicio3.TabIndex = 6;
+            // 
+            // Ejercicio4
+            // 
+            this.Ejercicio4.FormattingEnabled = true;
+            this.Ejercicio4.Location = new System.Drawing.Point(454, 63);
+            this.Ejercicio4.Name = "Ejercicio4";
+            this.Ejercicio4.Size = new System.Drawing.Size(102, 95);
+            this.Ejercicio4.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(585, 34);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(89, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Ejercicio5
+            // 
+            this.Ejercicio5.FormattingEnabled = true;
+            this.Ejercicio5.Location = new System.Drawing.Point(585, 63);
+            this.Ejercicio5.Name = "Ejercicio5";
+            this.Ejercicio5.Size = new System.Drawing.Size(102, 95);
+            this.Ejercicio5.TabIndex = 9;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(717, 34);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(89, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Ejercicio6
+            // 
+            this.Ejercicio6.FormattingEnabled = true;
+            this.Ejercicio6.Location = new System.Drawing.Point(717, 89);
+            this.Ejercicio6.Name = "Ejercicio6";
+            this.Ejercicio6.Size = new System.Drawing.Size(102, 69);
+            this.Ejercicio6.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(717, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(89, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(68, 186);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(89, 23);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(197, 186);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(89, 23);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(326, 186);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(89, 23);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(454, 186);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(89, 23);
+            this.button10.TabIndex = 16;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(585, 186);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(89, 23);
+            this.button11.TabIndex = 17;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(717, 186);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(89, 23);
+            this.button12.TabIndex = 18;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // Ejercicio7
+            // 
+            this.Ejercicio7.FormattingEnabled = true;
+            this.Ejercicio7.Location = new System.Drawing.Point(68, 215);
+            this.Ejercicio7.Name = "Ejercicio7";
+            this.Ejercicio7.Size = new System.Drawing.Size(102, 95);
+            this.Ejercicio7.TabIndex = 19;
+            // 
+            // Ejercicio8
+            // 
+            this.Ejercicio8.FormattingEnabled = true;
+            this.Ejercicio8.Location = new System.Drawing.Point(197, 215);
+            this.Ejercicio8.Name = "Ejercicio8";
+            this.Ejercicio8.Size = new System.Drawing.Size(102, 95);
+            this.Ejercicio8.TabIndex = 20;
+            // 
+            // Ejercicio9
+            // 
+            this.Ejercicio9.FormattingEnabled = true;
+            this.Ejercicio9.Location = new System.Drawing.Point(326, 215);
+            this.Ejercicio9.Name = "Ejercicio9";
+            this.Ejercicio9.Size = new System.Drawing.Size(102, 95);
+            this.Ejercicio9.TabIndex = 21;
+            // 
+            // Ejercicio10
+            // 
+            this.Ejercicio10.FormattingEnabled = true;
+            this.Ejercicio10.Location = new System.Drawing.Point(454, 215);
+            this.Ejercicio10.Name = "Ejercicio10";
+            this.Ejercicio10.Size = new System.Drawing.Size(102, 95);
+            this.Ejercicio10.TabIndex = 22;
+            // 
+            // Ejercicio11
+            // 
+            this.Ejercicio11.FormattingEnabled = true;
+            this.Ejercicio11.Location = new System.Drawing.Point(585, 215);
+            this.Ejercicio11.Name = "Ejercicio11";
+            this.Ejercicio11.Size = new System.Drawing.Size(102, 95);
+            this.Ejercicio11.TabIndex = 23;
+            // 
+            // Ejercicio12
+            // 
+            this.Ejercicio12.FormattingEnabled = true;
+            this.Ejercicio12.Location = new System.Drawing.Point(717, 215);
+            this.Ejercicio12.Name = "Ejercicio12";
+            this.Ejercicio12.Size = new System.Drawing.Size(102, 95);
+            this.Ejercicio12.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 390);
+            this.Controls.Add(this.Ejercicio12);
+            this.Controls.Add(this.Ejercicio11);
+            this.Controls.Add(this.Ejercicio10);
+            this.Controls.Add(this.Ejercicio9);
+            this.Controls.Add(this.Ejercicio8);
+            this.Controls.Add(this.Ejercicio7);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Ejercicio6);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.Ejercicio5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.Ejercicio4);
+            this.Controls.Add(this.Ejercicio3);
+            this.Controls.Add(this.Ejercicio2);
+            this.Controls.Add(this.ListaAleatorio);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox ListaAleatorio;
+        private System.Windows.Forms.ListBox Ejercicio2;
+        private System.Windows.Forms.ListBox Ejercicio3;
+        private System.Windows.Forms.ListBox Ejercicio4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox Ejercicio5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox Ejercicio6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.ListBox Ejercicio7;
+        private System.Windows.Forms.ListBox Ejercicio8;
+        private System.Windows.Forms.ListBox Ejercicio9;
+        private System.Windows.Forms.ListBox Ejercicio10;
+        private System.Windows.Forms.ListBox Ejercicio11;
+        private System.Windows.Forms.ListBox Ejercicio12;
+    }
+}
+
---
+++ WindowsFormsApplication10 - copia/WindowsFormsApplication10/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication10
+{
+    public partial class Form1 : Form
+    {
+        Random aleatorio = new Random();
+        int[] vector = new int[50];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListaAleatorio.Items.Clear();
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = aleatorio.Next(100);
+                ListaAleatorio.Items.Add(vector[i]);
+
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Ejercicio2.Items.Clear();
+            int menor = (0);
+            int mayor = (0);
+            for (int i = 0; i < vector[i]; i++)
+            {
+                if (vector[i] < menor)
+                {
+                    menor = vector[i];
+                }
+                else if (vector[i] > mayor)
+                {
+                    mayor = vector[i];
+                }
+
+            }
+            Ejercicio2.Items.Add("El mayor es:" + mayor);
+            Ejercicio2.Items.Add("El menor es:" + menor);
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Ejercicio3.Items.Clear();
+            Ejercicio3.Items.Add("Contenido 20: " + vector[20]);
+            Ejercicio3.Items.Add("Contenido 30: " + vector[30]);
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            Ejercicio4.Items.Clear();
+            int media = 0;
+            int media0 = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                media0 = media0 + vector[i];
+                media = media0 / 50;
+            }
+            Ejercicio4.Items.Add("La media es de: " + media);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+
+            Ejercicio5.Items.Clear();
+            int n=0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                for (int j = 0; j < vector.Length; j++)
+                {
+                    if(vector[j] < vector[i])
+                    {
+                        n = vector[i];
+                        vector[i] = vector[j];
+                        vector[j] = n;
+                    }
+                }
+            }
+                    for(int k = 0; k < vector.Length; k++)
+                    {
+                Ejercicio5.Items.Add(vector[k]);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Ejercicio6.Items.Clear();
+            int contador=0;
+            int valor = Convert.ToInt16(textBox1.Text);
+            for (int i=0; i<vector.Length;i++)
+            {
+                if(vector[i] == valor)
+                {
+                    contador++;
+                }
+            }
+            
+            try
+            {
+                int num = Convert.ToInt16(textBox1.Text);
+}
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show("valor incorrecto " + textBox1.Text);
+
+            }
+            Ejercicio6.Items.Add("Mi número: "+valor);
+            Ejercicio6.Items.Add("Aparece: " +contador);
+
+
+        }
+
+    }
+}
+   
